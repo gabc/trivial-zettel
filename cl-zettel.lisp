e8909e6707902606c574051635c14a313d5ac7b3
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2020-06-06 18:27:45 -0400

    Get nodes/text/no links

--- cl-zettel.lisp
+++ cl-zettel.lisp
@@ -2,15 +2,6 @@
 
 (in-package #:cl-zettel)
 
-(defun present-nodes (graph matches)
-  (let ((*present-depth* 0)
-        (*presented-nodes* (make-hash-table :test 'eq))
-        (*present-depth-increment* 0))
-    (with-output-to-string (s)
-      (do-leaf-nodes (n graph)
-        (if (cl-ppcre:all-matches matches (title-of n))
-            (org-present :flat n s))))))
-
 (defun format-dates (title)
   (cl-ppcre:regex-replace "%5D"
                           (cl-ppcre:regex-replace "\\*%5B" title "[")
@@ -19,3 +10,29 @@
   (cl-ppcre:regex-replace "\\]"
                           (cl-ppcre:regex-replace "\\[" date "\\\[")
                           "\\\]"))
+
+(defvar file #P"/Users/g/lib/org/Zettelkasten.org")
+
+(defun get-no-links (graph)
+  (let (nodes)
+    (cl-org-mode::do-leaf-nodes (n graph)
+      (if (not (cl-ppcre:all-matches "\\[\\[" (get-text n)))
+          (push n nodes)))
+    nodes))
+
+(defun find-node (graph text)
+  (cl-org-mode::do-leaf-nodes (n graph)
+    (if (or (cl-ppcre:all-matches text (cl-org-mode::node.out n))
+            (cl-ppcre:all-matches text (cl-org-mode::title-of n)))
+        (return-from find-node n))))
+(defun get-nodes (&optional file-arg)
+  (cl-org-mode::org-parse (or file-arg file)))
+
+(defun get-text (node)
+  (cond ((cl-org-mode::node.out node)
+         (cl-org-mode::node.out node))
+        ((and (cl-org-mode::section-of node)
+              (cl-org-mode::children-of
+               (cl-org-mode::section-of node)))
+         (car (cl-org-mode::children-of
+               (cl-org-mode::section-of node))))))
